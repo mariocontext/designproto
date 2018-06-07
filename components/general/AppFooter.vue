7fe3b4ebd645de15a1b0e36bdfb892d072227227
Mario Noble <mario@contextmultimedia.com>
2018-06-07 16:54:02 -0700

    moved hamburger nav to left, expanded main nav area, added bottom tab nav

---
+++ components/general/AppFooter.vue
@@ -0,0 +1,55 @@
+<template>
+
+  <footer class="footer app-footer" role="contentinfo">
+
+    <b-tabs type="is-toggle" size="is-medium" expanded>
+        <b-tab-item label="Pictures" icon="google-photos"></b-tab-item>
+        <b-tab-item label="Music" icon="library-music"></b-tab-item>
+        <b-tab-item label="Videos" icon="video"></b-tab-item>
+    </b-tabs>
+
+  </footer>
+
+  
+
+</template>
+
+
+
+<script>
+
+</script>
+
+
+
+<style lang="scss">
+
+.footer {
+  margin: 0;
+  padding:0;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+
+.app-footer {
+  
+  .tabs a {
+  flex-direction: column;
+  font-size: 1rem;
+    .icon:first-child {
+      margin-right: 0;
+    }
+  }
+
+  .tab-content {
+    padding: 0;
+  }
+
+  .tabs:not(:last-child) {
+    margin-bottom: 0;
+  }
+
+}
+
+</style>
---
+++ pages/design/mobileapp/mobiletest.vue
@@ -0,0 +1,22 @@
+<template>
+    <span>
+        <h1>My Mobile app exp</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem ratione, fugiat illo similique soluta ab dolore explicabo, numquam cupiditate, veritatis architecto molestias delectus vel quisquam laborum officia dignissimos deserunt tempora.</p>
+        <p>Eius temporibus tenetur accusantium possimus rem laborum id ex assumenda veniam? Veniam eius, exercitationem aperiam ipsum voluptatibus illo quidem tempore laborum, facilis consectetur iusto reiciendis hic sed dolores doloribus nam?</p>
+        <p>Ratione sit illum quaerat quo eligendi. Vero praesentium tempore commodi voluptas dolores repellendus quis odit maxime itaque esse cumque distinctio aliquam, voluptates reprehenderit ab consequatur sunt et earum eligendi nobis.</p>
+        <p>Unde, cum. Voluptatem, suscipit corrupti. Magnam fuga harum maxime amet ex animi suscipit tenetur. Mollitia, ipsa minus atque placeat porro ratione repellendus sint doloremque quo non odit? Consequuntur, architecto distinctio!</p>
+    </span>
+    
+</template>
+
+<script>
+export default {
+    layout: 'applayout'
+}
+</script>
+
+<style>
+
+</style>
+
+
